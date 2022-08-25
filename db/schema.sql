3281d023874d28eecb07ab2be453283df087ca18
Dixon <zdixon91.zd@gmail.com>
2022-08-24 21:50:19 -0500

    schema set-up

---
+++ db/schema.sql
@@ -0,0 +1,24 @@
+DROP DATABASE IF EXISTS employee;
+Create DATABASE employee;
+USE employee;
+
+Create TABLE department (
+    id INT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR (30) NOT NULL UNIQUE
+);
+
+CREATE TABLE role (
+    id INT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+    title VARCHAR (30) NOT NULL UNIQUE,
+    salary DECIMAL UNSIGNED NOT NULL,
+    department_id INT UNSIGNED NOT NULL,
+    INDEX department_id,
+);
+
+CREATE TABLE employee (
+    id INT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+    first_name VARCHAR (30) NOT NULL,
+    last_name VARCHAR (30) NOT NULL,
+    role_id INT UNSIGNED NOT NULL<
+    INDEX role_id,
+);
